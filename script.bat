d993184f9061774292cbe783c9e04e0e76ff5cae
TaeTanakrit0089 <65070089@kmitl.ac.th>
2024-06-25 22:07:22 +0700

    Added Files

--- script.bat
+++ script.bat
@@ -1,4 +1,16 @@
-mkdir -p Downloads/Files && \
-cd Downloads\Files
-curl -LO https://raw.githubusercontent.com/TaeTanakrit0089/Windows11KMITL/main/files/SarabunFont.zip
-curl -LO https://raw.githubusercontent.com/TaeTanakrit0089/Windows11KMITL/main/files/WinRAR.zip
+@echo off
+
+REM Directory to download files
+set DOWNLOAD_DIR=%USERPROFILE%\Downloads\Files
+
+REM Create the directory if it doesn't exist
+if not exist "%DOWNLOAD_DIR%" mkdir "%DOWNLOAD_DIR%"
+
+REM Download SarabunFont.zip
+curl -L "https://github.com/TaeTanakrit0089/Windows11KMITL/raw/main/files/SarabunFont.zip" -o "%DOWNLOAD_DIR%\SarabunFont.zip"
+
+REM Download WinRAR.zip
+curl -L "https://github.com/TaeTanakrit0089/Windows11KMITL/raw/main/files/WinRAR.zip" -o "%DOWNLOAD_DIR%\WinRAR.zip"
+
+echo Downloads completed.
+pause
