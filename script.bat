4901603aa65453b9a6f725c8ac863cb73720f780
TaeTanakrit0089 <65070089@kmitl.ac.th>
2024-06-25 22:03:27 +0700

    Added Files

---
+++ script.bat
@@ -0,0 +1,3 @@
+mkdir -p Downloads/Files && \
+curl -L "https://github.com/TaeTanakrit0089/Windows11KMITL/blob/main/files/SarabunFont.zip" -o Downloads/Files/SarabunFont.zip && \
+curl -L "https://github.com/TaeTanakrit0089/Windows11KMITL/blob/main/files/WinRAR.zip" -o Downloads/Files/WinRAR.zip 
